557fa982ec3870c5fe2ce32eb2afaee917048d93
Hyago Braga <40799862+bhyago@users.noreply.github.com>
2024-06-08 19:07:31 -0300

    http-busca-cep

---
+++ http-busca-cep/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep        string `json:"cep"`
+	Logradouro string `json:"logradouro"`
+	Bairro     string `json:"bairro"`
+	Localidade string `json:"localidade"`
+	Uf         string `json:"uf"`
+	Ibge       string `json:"ibge"`
+	Gia        string `json:"gia"`
+	Ddd        string `json:"ddd"`
+	Siafi      string `json:"siafi"`
+}
+
+func main() {
+	http.HandleFunc("/", BuscaCEPHandler)
+	http.ListenAndServe(":3333", nil)
+
+}
+
+func BuscaCEPHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		http.NotFound(w, r)
+		return
+	}
+
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	cep, error := BuscaCEP(cepParam)
+	if error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	//result, err := json.Marshal(cep)
+	//if err != nil {
+	// w.WriteHeader(http.StatusInternalServerError)
+	//}
+	//w.Write(result)
+	json.NewEncoder(w).Encode(cep)
+}
+
+func BuscaCEP(cep string) (*ViaCEP, error) {
+	resp, error := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+	if error != nil {
+		return nil, error
+	}
+
+	defer resp.Body.Close()
+	body, error := io.ReadAll(resp.Body)
+	if error != nil {
+		return nil, error
+	}
+	var c ViaCEP
+	error = json.Unmarshal(body, &c)
+	if error != nil {
+		return nil, error
+	}
+
+	return &c, nil
+}
